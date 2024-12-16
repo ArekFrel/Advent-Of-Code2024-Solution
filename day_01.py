60a54d9a57f8bf1bf33bfe795993f00a57ef5dae
Arkadiusz Frel <plfrela@tetrapak.com>
2024-12-16 21:12:03 +0100

    :day 1 done

--- day_01.py
+++ day_01.py
@@ -1,14 +1,38 @@
 DAY = __file__[-5:-3]
-FILE = f'../inputs/input_{DAY}.txt'
-# FILE = f'../inputs/test_input_{DAY}.txt'
+
+TEST = False
+if TEST:
+    FILE = f'./inputs/input_{DAY}.txt'
+else:
+    FILE = f'./inputs/test_input_{DAY}.txt'
 SOLVE_PART = 2
 
 
-def main():
-    rows = []
+def solve():
+    first_list, second_list = [], []
     with open(FILE, 'r', encoding='utf-8-sig') as my_input:
         for line in my_input:
-            rows.append(line.rstrip())
+            first, second = line.rstrip().split('   ')
+            first_list.append(int(first))
+            second_list.append(int(second))
+
+    if SOLVE_PART == 1:
+        first_list.sort()
+        second_list.sort()
+        result = 0
+        while first_list:
+            result += abs(first_list.pop() - second_list.pop())
+        print(f'Result of DAY {DAY}, part {SOLVE_PART} is {result}')
+
+    if SOLVE_PART == 2:
+        result = 0
+        for num in first_list:
+            result += num * second_list.count(num)
+        print(f'Result of DAY {DAY}, part {SOLVE_PART} is {result}')
+
+
+def main():
+    solve()
 
 
 if __name__ == '__main__':
